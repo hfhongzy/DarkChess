dbdff6e9db78338d6136b0a11f6c586cd178f4b5
Hongzy <297775278@qq.com>
2022-11-22 22:42:55 +0800

    hongzy-test

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
+        //Orz
         SwingUtilities.invokeLater(() -> {
             MainFrame mainFrame = new MainFrame(800,600);
             mainFrame.setVisible(true);
