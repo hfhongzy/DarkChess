b166697e0a5c45b24437c51a56809d47e03d00cc
ac-evil <2815352254@qq.com>
2022-11-25 00:42:54 +0800

    2022-11-25-01

---
+++ src/gameComponent/OptionalBox.java
@@ -0,0 +1,19 @@
+package gameComponent;
+
+import ButtonComponent.GameButton;
+
+import javax.swing.*;
+
+public class OptionalBox extends JComponent {
+    static final int WIDTH = 300;
+    static final int HEIGHT = 400;
+    public OptionalBox() {
+        setSize(WIDTH, HEIGHT);
+        GameButton btn1 = new GameButton(70, 40, "test");
+        btn1.setLocation(30, 40);
+        add(btn1);
+        GameButton btn2 = new GameButton(70, 100, "你好");
+        btn2.setLocation(150, 40);
+        add(btn2);
+    }
+}
