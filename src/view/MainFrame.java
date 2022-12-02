c7d8d437ad21c17c0d06b7aca5b6c7cfe5873f72
ac-evil <2815352254@qq.com>
2022-12-03 00:16:57 +0800

    2022-12-03-01

--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -9,6 +9,7 @@ public class MainFrame extends JFrame {
     private final int WIDTH;
     private final int HEIGHT;
     public Container pane; // 显示的 panel，用于切换多个页面
+    JPanel mainPanel, playGamePanel; // 对应的panel
     public MainFrame(int width, int height) {
         WIDTH = width;
         HEIGHT = height;
@@ -24,16 +25,15 @@ public class MainFrame extends JFrame {
         pane.setPreferredSize(new Dimension(width, height));
 
         // 初始化 panel
-        JPanel mainPanel = new MainPanel(this);
-        JPanel playGamePanel = new PlayGamePanel();
-
+        mainPanel = new MainPanel(this);
+        playGamePanel = new PlayGamePanel();
+        // 加入 panel
         pane.add(PanelType.MAIN_PANEL, mainPanel);
         pane.add(PanelType.PLAY_GAME_PANEL, playGamePanel); // 名称 - panel
-
+        // 展示当前 panel
         showPanel(PanelType.MAIN_PANEL);
-//        ((CardLayout) pane.getLayout()).show(pane, PanelType.MAIN_PANEL); // 展示 card
 
-        pack();
+        pack(); // 自动调整大小
         setLocationRelativeTo(null); // 窗口居中
     }
     public void showPanel(String panelType) {
--- src/view/MainPanel.java
+++ src/view/MainPanel.java
@@ -1,20 +1,29 @@
 package view;
 
 import ButtonComponent.GameButton;
+import LabelComponent.GameLabel;
+import LabelComponent.TextBlock;
 import model.PanelType;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainPanel extends JPanel {
     MainFrame mainFrame;
     public MainPanel(MainFrame mainFrame) {
         setLayout(null);
         this.mainFrame = mainFrame;
-        GameButton btn1 = new GameButton(100, 40, "aaa", true, () -> mainFrame.showPanel(PanelType.PLAY_GAME_PANEL));
-        GameButton btn2 = new GameButton(100, 40, "bbb", false, null);
+        GameButton btn1 = new GameButton(100, 40, "aaa", true, () -> {
+//            mainFrame.playGamePanel.xxx
+            mainFrame.showPanel(PanelType.PLAY_GAME_PANEL);
+        });
+        GameButton btn2 = new GameButton(100, 40, "___", false, null);
         btn1.setLocation(300, 100);
-        btn2.setLocation(300, 400);
+        btn2.setLocation(500, 100);
         add(btn1);
         add(btn2);
+        GameLabel label = new GameLabel(new TextBlock("L", 40), new TextBlock("iu_AK", Color.RED, 20));
+        label.setLocation(10, 10);
+        add(label);
     }
 }
