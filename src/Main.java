999e3430bbb00beab5c9931e87649ad5ecc356d6
Hongzy <297775278@qq.com>
2022-12-02 22:04:52 +0800

    2022-12-2

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
+        
         System.out.println(args.length);
         //Orz System.getProperty()
         
