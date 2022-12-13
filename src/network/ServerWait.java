0969a72140442a80fa781dea8f3f62f56991b083
Hongzy <297775278@qq.com>
2022-12-13 23:35:19 +0800

    2022-12-10

---
+++ src/network/ServerWait.java
@@ -0,0 +1,41 @@
+package network;
+
+import gameController.OnlineGameController;
+import model.PanelType;
+import view.MainFrame;
+import view.PlayGamePanel;
+
+public class ServerWait extends Thread{
+  Thread thread;
+  PlayGamePanel playGamePanel;
+  MainFrame mainFrame;
+  public ServerWait(MainFrame mainFrame, PlayGamePanel playGamePanel) {
+    this.mainFrame = mainFrame;
+    this.playGamePanel = playGamePanel;
+  }
+  public void start() {
+    //Message.show("连接中！");
+    if(thread == null) {
+      thread = new Thread(this, "ServerThread");
+      thread.start();
+    }
+  }
+  OnlineGameController onlineGameController;
+  
+  @Override
+  public void interrupt() {
+    onlineGameController.interrupt();
+  }
+  
+  public void run() {
+    onlineGameController = new OnlineGameController(true);
+    onlineGameController.construct();
+    onlineGameController.setMode(true, true, true, true);
+    playGamePanel.setGameController(onlineGameController);
+    onlineGameController.addControllerToNet();
+    if(onlineGameController.isConnected()) {
+      playGamePanel.start();
+      mainFrame.showPanel(PanelType.PLAY_GAME_PANEL);
+    }
+  }
+}
